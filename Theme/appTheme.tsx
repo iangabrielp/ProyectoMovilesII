62a33154d1aecfd914077eee7ea5ee275dfd32e1
CrasherG4 <cgp4762@gmail.com>
2025-01-08 10:50:57 -0500

    conexión base de datos

---
+++ Theme/appTheme.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  //Diseños generales
+  contenedorAll:{
+    flex:1,
+    padding:20
+  }
+  //Diseños del Registro
+})
---
+++ screens/LogInScreen.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function LogInScreen() {
+  return (
+    <View>
+      <Text>LogInScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
