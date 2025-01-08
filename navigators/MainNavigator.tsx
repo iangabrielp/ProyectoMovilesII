24cca186616542687ace24d3b1db9646d1179c1e
Your Name <you@example.com>
2025-01-07 20:27:58 -0500

    MainNavigator

---
+++ navigators/MainNavigator.tsx
@@ -0,0 +1,12 @@
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  );
+}
