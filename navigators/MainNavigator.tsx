06a82554bbb582d0489afa7173bba07a4ab49f47
Your Name <you@example.com>
2025-01-07 20:51:09 -0500

    Welcome

--- navigators/MainNavigator.tsx
+++ navigators/MainNavigator.tsx
@@ -1,12 +1,23 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
 
 const Stack = createStackNavigator();
 
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Home" component={WelcomeScreen} />
+      
     </Stack.Navigator>
+  );
+}
+
+export default function MainNavigator(){
+  return(
+      <NavigationContainer>
+          <MyStack/>
+      </NavigationContainer>
+
   );
 }
---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,30 @@
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function WelcomeScreen({navigation} : any) {
+  return (
+    <ImageBackground 
+    source={{uri:'https://4kwallpapers.com/images/walls/thumbs_3t/20503.jpg'}} 
+    style={styles.container}
+    >
+      <Text style={{fontSize:30, color:'white'}}>Welcome</Text>
+      <Button 
+       title='LOGIN'
+       onPress={()=> navigation.navigate('Bottom')}
+       />
+       <Button 
+       title='REGISTRATE'
+       onPress={()=> navigation.navigate('Bottom')}
+       />
+
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
